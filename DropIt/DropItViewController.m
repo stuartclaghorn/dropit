7d9ddf8d93116e265818018c7a19488e2ab680eb
Stuart Claghorn <sclaghorn@gmail.com>
2014-07-27 16:34:06 -0400

    Initial creation from Xcode - first time

--- DropIt/DropItViewController.m
+++ DropIt/DropItViewController.m
@@ -9,10 +9,43 @@
 #import "DropItViewController.h"
 
 @interface DropItViewController ()
+@property (weak, nonatomic) IBOutlet UIView *gameView;
 
 @end
 
 @implementation DropItViewController
+
+static const CGSize DROP_SIZE = {40, 40};
+
+- (IBAction)tap:(UITapGestureRecognizer *)sender
+{
+    [self drop];
+}
+
+- (void) drop
+{
+    CGRect frame;
+    frame.origin = CGPointZero;
+    frame.size = DROP_SIZE;
+    int x = (arc4random()%(int)self.gameView.bounds.size.width) / DROP_SIZE.width;
+    frame.origin.x = x * DROP_SIZE.width;
+    
+    UIView *dropView = [[UIView alloc] initWithFrame: frame];
+    dropView.backgroundColor = [self randomColor];
+    [self.gameView addSubview:dropView];
+}
+
+- (UIColor *)randomColor
+{
+    switch (arc4random()%5){
+        case 0: return [UIColor greenColor];
+        case 1: return [UIColor blueColor];
+        case 2: return [UIColor orangeColor];
+        case 3: return [UIColor redColor];
+        case 4: return [UIColor purpleColor];
+    }
+    return [UIColor blackColor];
+}
 
 - (void)viewDidLoad
 {
